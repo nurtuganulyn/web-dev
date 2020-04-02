ce1fea5b6233d262b0a56af88da891ecc925c82d
Nursultan <nurtuganulyn@gmail.com>
2020-04-02 20:09:40 +0600

    folder is deleted

--- shop_back/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from api.views import products, get_product, categories, get_category, get_category_products
-urlpatterns = [
-    path('products/', products),
-    path('products/<int:id>/', get_product),
-    path('categories/', categories),
-    path('categories/<int:id>/', get_category),
-    path('categories/<int:id>/products/', get_category_products),
-]
