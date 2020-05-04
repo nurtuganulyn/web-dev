5344952d6563f5915349a63697fdb40a075ad458
Nursultan <nurtuganulyn@gmail.com>
2020-05-04 16:56:33 +0600

    Delete

--- front-end/src/app/clothes.ts
+++
@@ -1,9 +0,0 @@
-export interface Clothes {
-    id: number;
-    name: string;
-    imageLink: string;
-    imageLink2: string;
-    price: string;
-    description: string;
-    category: number;
-}
--- front-end/src/app/models/user.ts
+++
@@ -1,7 +0,0 @@
-export interface User {
-    user_id: number;
-    name?: string;
-    login: string;
-    email: string;
-    password: string;
-}
